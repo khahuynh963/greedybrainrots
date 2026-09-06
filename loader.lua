993dc8e302856cf19c675cd6ec72d32d32db8996
khahuynh369 <huynhkha20022001@gmail.com>
2026-09-07 04:53:15 +0700

    V16: Smart Auto Plant on Single Grow Pad & Lightning Dodge ~1s before strike

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 --[[
     ===================================================================
-    🧠 GREEDY BRAINROTS - ULTIMATE AUTO HUB (V14 FAST LOADER)
+    🧠 GREEDY BRAINROTS - ULTIMATE AUTO HUB (V16 FAST LOADER)
     ===================================================================
 --]]
 pcall(function()
@@ -13,7 +13,8 @@ pcall(function()
     end
 end)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/khahuynh963/greedybrainrots/main/v15.lua?" .. math.random(1, 999999)))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/khahuynh963/greedybrainrots/main/v16.lua?" .. math.random(1, 999999)))()
+
 
 
 

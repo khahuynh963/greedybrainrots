c8c93010d1af6c1ca733b0f9cc3753735de4fc2a
khahuynh369 <huynhkha20022001@gmail.com>
2026-09-07 04:35:35 +0700

    Release V14 with Auto Select Food system

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 --[[
     ===================================================================
-    🧠 GREEDY BRAINROTS - ULTIMATE AUTO HUB (V13 FAST LOADER)
+    🧠 GREEDY BRAINROTS - ULTIMATE AUTO HUB (V14 FAST LOADER)
     ===================================================================
 --]]
 pcall(function()
@@ -13,6 +13,7 @@ pcall(function()
     end
 end)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/khahuynh963/greedybrainrots/main/v13.lua?" .. math.random(1, 999999)))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/khahuynh963/greedybrainrots/main/v14.lua?" .. math.random(1, 999999)))()
+
 
 

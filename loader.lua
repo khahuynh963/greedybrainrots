27fea7c70f7cb713c98928568186076580deefc8
khahuynh369 <huynhkha20022001@gmail.com>
2026-09-07 05:36:15 +0700

    V25: Dual-Layer Anti-AFK engine (Idled event + 3-min proactive heartbeat)

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 --[[
     ===================================================================
-    🧠 GREEDY BRAINROTS - ULTIMATE AUTO HUB (V24 FAST LOADER)
+    🧠 GREEDY BRAINROTS - ULTIMATE AUTO HUB (V25 FAST LOADER)
     ===================================================================
 --]]
 pcall(function()
@@ -14,4 +14,4 @@ pcall(function()
     end
 end)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/khahuynh963/greedybrainrots/main/v24.lua?" .. math.random(1, 999999)))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/khahuynh963/greedybrainrots/main/v25.lua?" .. math.random(1, 999999)))()

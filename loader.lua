e4eafef2a59f2f999221a7d25b0427b0176b0b61
khahuynh369 <huynhkha20022001@gmail.com>
2026-09-07 04:30:30 +0700

    Release V13 with Lightning Dodge & Optimal Harvest system

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 --[[
     ===================================================================
-    🧠 GREEDY BRAINROTS - ULTIMATE AUTO HUB (V12 FAST LOADER)
+    🧠 GREEDY BRAINROTS - ULTIMATE AUTO HUB (V13 FAST LOADER)
     ===================================================================
 --]]
 pcall(function()
@@ -13,5 +13,6 @@ pcall(function()
     end
 end)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/khahuynh963/greedybrainrots/main/v12.lua?" .. math.random(1, 999999)))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/khahuynh963/greedybrainrots/main/v13.lua?" .. math.random(1, 999999)))()
+
 
